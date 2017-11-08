870cc68a806ed60649407b500ca273a70826e2db
Unknown <j.r.kosinski@gmail.com>
2017-11-08 17:34:31 +0700

    code cleanup

---
+++ makeclean.sh
@@ -0,0 +1,3 @@
+make clean
+make
+sudo make flash ESPPORT=/dev/ttyUSB0
---
+++ storage.h
@@ -0,0 +1,20 @@
+#ifndef __STORAGE_H__
+#define __STORAGE_H__
+
+static char* getWifiPasswd() {
+	return 0;
+}
+
+static char* getWifiUsername() {
+	return 0;
+}
+
+static int setWifiPasswd(const char* value) {
+	return 0;
+}
+
+static int setWifiUsername(const char* value) {
+	return 0;
+}
+
+#endif 
